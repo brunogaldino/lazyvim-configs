21afcc54015fd38dd62e983c64088996d1e886da
Bruno Galdino <brunogaldinoc@gmail.com>
2025-05-25 16:37:03 -0300

    wip

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -28,7 +28,10 @@ vim.opt.undofile = true -- enable persistent undo
 vim.opt.updatetime = 100 -- faster completion (4000ms default)
 vim.opt.writebackup = false -- if a file is being edited by another program (or was written to file while editing with another program), it is not allowed to be edited
 vim.opt.expandtab = true -- convert tabs to spaces
-vim.opt.shiftwidth = 2 -- the number of spaces inserted for each indentation
+vim.o.tabstop = 4 -- A TAB character looks like 4 spaces
+vim.opt.softtabstop = 4 -- Number of spaces inserted instead of a TAB character
+vim.opt.shiftwidth = 4 -- Number of spaces inserted when indenting
+vim.opt.shiftwidth = 4 -- the number of spaces inserted for each indentation
 vim.opt.cursorline = true -- highlight the current line
 vim.opt.number = true -- set numbered lines
 vim.opt.breakindent = true -- wrap lines with indent
@@ -59,4 +62,3 @@ vim.opt.fillchars = { eob = " " } -- change the character at the end of buffer
 -- If using transparent backgroud
 -- vim.opt.pumblend = 0
 vim.g.lazyvim_prettier_needs_config = true
--- vim.g.trouble_lualine = false
